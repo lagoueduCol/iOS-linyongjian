175ea51b1607f018ca16a0028421d8e8ad02e739
Jake Lin <jakelinau@gmail.com>
2020-10-15 17:10:37 +1100

    Create a new iOS project

---
+++ Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by Jake Lin on 15/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
