539c3ce1a733ed270e7c54fde6589620e6dcd235
Jake Lin <jakelinau@gmail.com>
2020-10-20 17:28:26 +1100

    Add `accessoryType` to `InternalMenuActionTriggerCell`

--- Moments/Moments/Features/InternalMenu/Views/InternalMenuActionTriggerCell.swift
+++ Moments/Moments/Features/InternalMenu/Views/InternalMenuActionTriggerCell.swift
@@ -8,18 +8,10 @@
 import UIKit
 
 class InternalMenuActionTriggerCell : InternalMenuCell<InternalMenuActionTriggerItemViewModel> {
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     var item: InternalMenuActionTriggerItemViewModel?
 
     override func update(_ item: InternalMenuActionTriggerItemViewModel) {
+        accessoryType = .disclosureIndicator
         self.item = item
         textLabel?.text = item.title
         textLabel?.textColor = item.titleColor
