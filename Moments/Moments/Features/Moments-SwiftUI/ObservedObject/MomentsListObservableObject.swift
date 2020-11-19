438c1fa24eae1dd85b262b0abb2be4a62f390e03
Jake Lin <jakelinau@gmail.com>
2020-11-20 00:40:13 +1100

    Add `MomentsListObservableObject` to bridge RxSwift and Combine

---
+++ Moments/Moments/Features/Moments-SwiftUI/ObservedObject/MomentsListObservableObject.swift
@@ -0,0 +1,48 @@
+//
+//  MomentsListObservableObject.swift
+//  Moments
+//
+//  Created by Jake Lin on 19/11/20.
+//
+
+import Foundation
+import Combine
+import RxSwift
+
+struct IdentifiableListItemViewModel: Identifiable {
+    var id = UUID()
+    var viewModel: ListItemViewModel
+}
+
+final class MomentsListObservableObject: ObservableObject {
+    private let viewModel: MomentsListViewModel
+    private let disposeBag: DisposeBag = .init()
+
+    @Published var listItems: [IdentifiableListItemViewModel] = []
+
+    init(userID: String,
+         momentsRepo: MomentsRepoType) {
+        viewModel = MomentsListViewModel(userID: userID, momentsRepo: momentsRepo)
+
+        setupBindings()
+    }
+
+    func loadItems() {
+        viewModel.loadItems()
+            .subscribe()
+            .disposed(by: disposeBag)
+    }
+}
+
+private extension MomentsListObservableObject {
+    func setupBindings() {
+        viewModel.listItems
+            .do(onNext: { [weak self] items in
+                guard let self = self else { return }
+                self.listItems.removeAll()
+                self.listItems.append(contentsOf: items.flatMap { $0.items }.map { IdentifiableListItemViewModel(viewModel: $0) })
+            })
+            .subscribe()
+            .disposed(by: disposeBag)
+    }
+}
