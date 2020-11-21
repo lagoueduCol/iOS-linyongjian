62153e099aad726b4e5810dd130c9a47cfd5622d
Jake Lin <jakelinau@gmail.com>
2020-11-21 22:35:39 +1100

    Implement `SwiftUIMomentListItemView`

--- Moments/Moments/Features/Moments-SwiftUI/Views/SwiftUIMomentListItemView.swift
+++ Moments/Moments/Features/Moments-SwiftUI/Views/SwiftUIMomentListItemView.swift
@@ -6,13 +6,52 @@
 //
 
 import SwiftUI
+import struct Kingfisher.KFImage
+import DesignKit
 
 struct SwiftUIMomentListItemView: View {
     let viewModel: MomentListItemViewModel
 
     var body: some View {
-        Text(viewModel.userName)
-            .frame(height: 120)
+        HStack(alignment: .top, spacing: Spacing.medium) {
+            KFImage(viewModel.userAvatarURL)
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 44, height: 44)
+                    .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                .padding(.leading, Spacing.medium)
+
+            VStack(alignment: .leading) {
+                Text(viewModel.userName)
+                    .font(.subheadline)
+                    .foregroundColor(.primary)
+
+                if let title = viewModel.title {
+                    Text(title)
+                        .font(.body)
+                        .foregroundColor(Color.secondary)
+                }
+
+                if let photoURL = viewModel.photoURL {
+                    KFImage(photoURL)
+                        .resizable()
+                        .frame(width: 240, height: 120)
+                }
+
+                if let postDateDescription = viewModel.postDateDescription {
+                    Text(postDateDescription)
+                        .font(.footnote)
+                        .foregroundColor(Color.secondary)
+                }
+            }
+            Spacer()
+        }
+        .frame(maxWidth:.infinity)
+        .padding(EdgeInsets(top: Spacing.medium, leading: 0, bottom: Spacing.medium, trailing: 0))
+        .background(BlurView(style: .systemMaterial))
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color.black.opacity(0.15), radius: 20, x: 0, y: 20)
+        .padding(.horizontal)
     }
 }
 
