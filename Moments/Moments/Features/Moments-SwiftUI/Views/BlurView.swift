4b5275ef85955cddba3e376da22723f492368daa
Jake Lin <jakelinau@gmail.com>
2020-11-21 19:53:57 +1100

    Add `BlurView`

---
+++ Moments/Moments/Features/Moments-SwiftUI/Views/BlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurView.swift
+//  Moments
+//
+//  Created by Jake Lin on 21/11/20.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
