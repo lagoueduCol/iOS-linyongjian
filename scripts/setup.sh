a2712c78e4020a94eb3e449fed664698a17b135d
Jake Lin <jakelinau@gmail.com>
2020-12-31 12:41:15 +1100

    Add ruby installation in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+
+# Install ruby using rbenv
+ruby_version=`cat .ruby-version`
+if [[ ! -d "$HOME/.rbenv/versions/$ruby_version" ]]; then
+  rbenv install $ruby_version;
+fi
+
+# Install bunlder
 gem install bundler
 
+# Install all gems
 bundle install
 
+# Install all pods
 bundle exec pod install
