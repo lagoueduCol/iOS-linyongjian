5aea94f7aecf6e7e1cdfc96b3b616bf6c4d580b1
Jake Lin <jakelinau@gmail.com>
2021-05-05 23:15:46 +1000

    Remove unnecessary `ScreenviewsTrackingEventType`

--- Moments/Moments/Foundations/Analytics/Common/ScreenviewsTrackingEvent.swift
+++ Moments/Moments/Foundations/Analytics/Common/ScreenviewsTrackingEvent.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct ScreenviewsTrackingEvent: ScreenviewsTrackingEventType {
+struct ScreenviewsTrackingEvent: TrackingEventType {
     let screenName: String
     let screenClass: String
 }
